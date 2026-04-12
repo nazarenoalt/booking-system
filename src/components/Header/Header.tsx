2ac793ad34274493fc165633eaa15fce97c6e42a
Nazareno Aznar Altamiranda <nazareno.aznar@gmail.com>
2026-04-12 20:50:56 -0300

    feat: header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="bg-blue-300 h-32 flex p-8 ">
+      <div className="h-full flex justify-center items-center">
+        <h1 className="text-4xl">Clinic Booking System</h1>
+      </div>
+    </header>
+  );
+}
